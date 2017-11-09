9b1bf708e581743865aaed49aadd76794768570d
Robert Andersen <robert.andersen@uoit.net>
2017-11-09 15:01:40 -0500

    Did some class stuff

--- PlantsvZombies/PlantsvZombies/peaShooter.h
+++ PlantsvZombies/PlantsvZombies/peaShooter.h
@@ -1,5 +1,5 @@
 #pragma once
-class peaShooter
+class peaShooter 
 {
 public:
 	peaShooter();
--- PlantsvZombies/PlantsvZombies/plant.h
+++ PlantsvZombies/PlantsvZombies/plant.h
@@ -1,1 +1,9 @@
 #pragma once
+#include "Object.h"
+
+class plant : public Object
+{
+public:
+	plant();
+	~plant();
+};
