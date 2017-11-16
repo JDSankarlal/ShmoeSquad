b46cc63f69de8db545ff58021120cdcbb870b11d
Josh Sankarlal <joshua.sankarlal@uoit.net>
2017-11-16 13:50:16 -0500

    Got rid of test sprite in main -- commented it out

--- PlantsvZombies/PlantsvZombies/main.cpp
+++ PlantsvZombies/PlantsvZombies/main.cpp
@@ -16,9 +16,11 @@ static GameHandler game;
 void main()
 
 {
-	Sprite zombie;
+	/*Sprite zombie;
 	zombie.init('%')->setPosition(COORD{ 10,10 });
+	*/ 
 	game.printDisplay();
+	
 	clock_t SUN_TIME,ZOMBIE_TIME,WORLD_CLOCK;  //Saves two seperate time variables at runtime, modifies each individually below
 	SUN_TIME = clock(); 
 	ZOMBIE_TIME = clock();
@@ -26,11 +28,10 @@ void main()
 
 	while (true) {
 
-	zombie.draw();
+	  //zombie.draw();
 	 //Program run time at this point saved in variable GAME_TIME
 	//gameconsole.Initialize(vec2(120, 200), "Plants vs Zombies");
 	//cout << GAME_TIME << endl;
-
 	if (((clock() - SUN_TIME) / CLOCKS_PER_SEC) >= 10.0f)  //Spawns a sun from SKY every 10 seconds. Will have to figure out how to spawn it every 10 local seconds relative to individual sunflowers spawn time
 	{
 		game.createSun();
