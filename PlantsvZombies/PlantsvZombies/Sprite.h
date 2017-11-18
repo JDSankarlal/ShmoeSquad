6bd28cf8b3476086e9e9f0a9d3d7fc6bceed8a45
Devon Poon Woo <devon.poonwoo@uoit.net>
2017-11-18 13:29:53 -0500

    Modified the way text files are imported for sprite data. Now sprites of any dimensions can be used.

--- PlantsvZombies/PlantsvZombies/Sprite.h
+++ PlantsvZombies/PlantsvZombies/Sprite.h
@@ -4,8 +4,8 @@
 #include <cstdio>
 #include <fstream>
 #include <string>
-#define numColumns 7//height of Sprites
-#define numRows 6//width of Sprites, all sprites are the same size
+//#define numColumns 7//height of Sprites
+//#define numRows 6//width of Sprites, all sprites are the same size
 using std::ifstream;
 using std::string;
 
@@ -25,12 +25,32 @@ public:
 		file.open(fileName);
 		file >> std::noskipws;//tells it not to skip white space while scanning file
 		int charNum = 0;
-		int line = 1;
+		int line = 0;
 		char c;
-		while (file >> c){
-			ascii[line][charNum] = c;
-			charNum++;
+		numRows = 0;
+		numCols = 1;
+		while (file >> c) {//gets the number of rows and columns in the file to set the size of the array to store the data itn
 			if (c == '\n') {
+				charNum = 0;
+				numCols++;
+			}
+			else {
+				charNum++;
+			}
+		}
+		numRows = charNum;
+		ascii = new char* [numCols];//allocates memory for an array of pointers
+		for (int i = 0; i < numCols; i++) {//allocates memory for each pointer in the above array to be an array of characters
+			ascii[i] = new char [numRows];
+		}
+		charNum = 0;
+		file.close();
+		file.open(fileName);
+		char c2;
+		while (file >> c2){
+			ascii[line][charNum] = c2;
+			charNum++;
+			if (c2 == '\n') {
 				charNum = 0;
 				line++;
 			}
@@ -40,7 +60,7 @@ public:
 		setPosition(startPos);
 	}
 
-	void init(const char data[numRows][numColumns]) {//set what the sprite looks like
+	/*void init(const char data[numRows][numColumns]) {//set what the sprite looks like
 		for (unsigned int i = 0; i < numRows; i++) {
 			for (unsigned int j = 0; j < numColumns; j++) {
 				ascii[i][j] = data[i][j];
@@ -48,7 +68,7 @@ public:
 		}
 		COORD startPos{ 0,0 };
 		setPosition(startPos);
-	}
+	}*/
 
 	void updatePosition() {//updates position to draw the sprite at
 		position.X = position.X + movePosition.X;
@@ -61,15 +81,17 @@ public:
 
 	void draw() {//draw sprite to screen
 		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
-		for (SHORT i = 0; i < numRows; i++) {
-			for (SHORT j = 0; j < numColumns; j++) {
+		for (SHORT i = 0; i < numCols; i++) {
+			SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{ position.X, position.Y + i });
+			for (SHORT j = 0; j < numRows; j++) {
 				printf("%c", ascii[i][j]);
 			}
-			SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{ position.X, position.Y + i });
 		}
 	}
 
 private:
 	COORD position;//Sprites current position
-	char ascii[numRows][numColumns];
+	char** ascii;
+	int numRows = 0;
+	int numCols = 0;
 };
